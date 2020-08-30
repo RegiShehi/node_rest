ebc1ec8de3956fc8f80b69ff5f0882e04e450df6
Regi Shehi <shehi.regi@gmail.com>
2020-08-30 17:39:45 +0200

    Add mongo db abd post object model

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    'mongodb+srv://regi:NvkgD3Hok1E4F46I@cluster0.n7bqv.mongodb.net/messages?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(8000);
+  })
+  .catch((error) => console.log(error));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,49 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: 1,
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/bonum_pikat.png',
+        creator: {
+          name: 'Regi',
+        },
+        date: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: 'Validation failed', errors: errors.array() });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/bonum_pikant.png',
+    creator: { name: 'Regi' },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result,
+      });
+    })
+    .catch((error) => console.log(error));
 };
