b1bc665e9d8ad3174656991112e4f216fbac2eac
Regi Shehi <shehi.regi@gmail.com>
2020-08-31 20:30:32 +0200

    Upload images

--- app.js
+++ app.js
@@ -2,12 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, res, next) => {
+    cb(null, 'images');
+  },
+  filename: (req, res, cb) => {
+    cb(null, new Date().toISOString + '-' + fileStorage.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,40 +3,40 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1,
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/bonum_pikat.png',
-        creator: {
-          name: 'Regi',
-        },
-        date: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ message: 'Fetched posts', posts: posts });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+
+      next(error);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    // const error = new Error('Validation failed');
-    // error.statusCode = 422;
-    // error.errors = errors.array();
-    // throw error;
     return res
       .status(422)
       .json({ message: 'Validation failed', errors: errors.array() });
   }
 
+  if (!req.file) {
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
+
   const title = req.body.title;
   const content = req.body.content;
+  const imageUrl = req.file.path;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/bonum_pikant.png',
+    imageUrl: imageUrl,
     creator: { name: 'Regi' },
   });
   post
@@ -46,6 +46,29 @@ exports.createPost = (req, res, next) => {
         message: 'Post created successfully!',
         post: result,
       });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+
+      next(error);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        // error.errors = errors.array();
+        throw error;
+      }
+
+      res.status(200).json({ message: 'Post fetched', post: post });
     })
     .catch((error) => {
       if (!error.statusCode) {
